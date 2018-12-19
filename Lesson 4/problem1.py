dc55e74456f6a18c5f403f372d0428b7bff1c163
sgalicia <44712840+sgalicia@users.noreply.github.com>
2018-12-19 14:48:05 -0500

    Add files via upload

---
+++ Lesson 4/problem1.py
@@ -0,0 +1,4 @@
+initial_score = 2.9
+new_score = initial_score / 4 * 100
+new_score = round (new_score)
+print (new_score)
---
+++ Lesson 4/problem3.py
@@ -0,0 +1,8 @@
+
+english = 89 
+math = 95
+history = 87
+science = 84
+
+average = (english+math+history+science)/4
+print(average)
